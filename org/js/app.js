91ffcfae1d07a138ba33cee7901d7fe6c319ab46
MagRelo <Magrelo404@gmail.com>
2014-12-16 11:03:12 -0700

    formatting and add spinner

--- org/js/app.js
+++ org/js/app.js
@@ -4,24 +4,23 @@ var myAppModule = angular.module('myApp', []);
 myAppModule
   .controller('MainController', function($scope, $http) {
 
+    $scope.showLoader = false;
     $scope.weather = null;
     $scope.city = "Boise";
     $scope.state = "ID";
 
 
     $scope.getWeather = function(){
+
+      $scope.showLoader = true;
+
       $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + $scope.city + '%2C%20' + $scope.state + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
         .then(function(result){
+
+          $scope.showLoader = false;
           $scope.weather = result.data.query.results.channel;
         });
     };
-
-
-
-
-
-
-
 
   })
 
