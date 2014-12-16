ff4be71fc1dc96fabe949c9b0d192ed9f065e958
MagRelo <Magrelo404@gmail.com>
2014-12-16 10:14:47 -0700

    add weather data

--- org/js/app.js
+++ org/js/app.js
@@ -3,11 +3,13 @@ var myAppModule = angular.module('myApp', []);
 
 // configure the module.
 // in this example we will create a greeting filter
-myAppModule.controller('MainController', function($scope) {
-  $scope.list = [
-    {name: 'Matt'},
-    {name: 'Bill'},
-    {name: 'Ali'},
-    {name: 'Jim'}
-  ]
+myAppModule.controller('MainController', function($scope, $http) {
+
+  $http.get('http://api.openweathermap.org/data/2.5/weather?lat=46.5&lon=-116&units=imperial')
+    .then(function(result){
+
+      $scope.weather = result.data;
+    });
+
+
 });
