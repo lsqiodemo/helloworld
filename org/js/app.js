3947d241332602dc10c15a8a404c26eb3f1f8322
MagRelo <Magrelo404@gmail.com>
2014-12-16 10:33:48 -0700

    format weather dat

--- org/js/app.js
+++ org/js/app.js
@@ -8,7 +8,7 @@ myAppModule
     $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
       .then(function(result){
 
-        $scope.weather = result.data;
+        $scope.weather = result.data.query.results.channel;
       });
   })
 
