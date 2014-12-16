902b9934c7953f9fb4c3f002350a00e7c7ae2180
MagRelo <Magrelo404@gmail.com>
2014-12-16 13:48:22 -0700

    add animation

--- org/js/clients/clients.controller.js
+++ org/js/clients/clients.controller.js
@@ -4,9 +4,15 @@ angular.module("AngularTestApp")
     $scope.showLoader = false;
     $scope.clients = null;
 
+    var lsqObject = {
+      "token" : "123456"
+      ,"request" : "read"
+      ,"query": {}
+    };
+
     $scope.getClients = function(){
       $scope.showLoader = true;
-      $http.get('')
+      $http.post('https://helloworld.lsq.io/api/v1/client', lsqObject)
         .then(function(result){
           $scope.showLoader = false;
           $scope.clients = result.data;
