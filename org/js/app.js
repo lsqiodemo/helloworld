318be9f0e578af0583f7f6b6990f531c75602e18
MagRelo <Magrelo404@gmail.com>
2014-12-16 16:25:46 -0700

    add events

--- org/js/app.js
+++ org/js/app.js
@@ -6,14 +6,17 @@ angular.module('AngularTestApp', ['ngRoute', 'ngAnimate'])
       .when('/', {
         templateUrl: 'js/weather/weather.html'
       })
-      .when('/weather', {
-        templateUrl: 'js/weather/weather.html'
-      })
       .when('/clients', {
         templateUrl: 'js/clients/clients.html'
       })
       .when('/schemas', {
         templateUrl: 'js/schemas/schemas.html'
+      })
+      .when('/events', {
+        templateUrl: 'js/events/events.html'
+      })
+      .when('/weather', {
+        templateUrl: 'js/weather/weather.html'
       })
       .otherwise({
         redirectTo: '/'
---
+++ org/js/events/events.controller.js
@@ -0,0 +1,18 @@
+angular.module("AngularTestApp")
+  .controller('EventsController', function($scope, lsqService) {
+
+    $scope.showLoader = false;
+    $scope.clients = null;
+
+    $scope.getEvents = function(){
+      $scope.showLoader = true;
+
+      lsqService.getCollection('events')
+        .then(function(result){
+          $scope.showLoader = false;
+          $scope.events = result.data.result;
+        });
+
+    };
+
+  });
