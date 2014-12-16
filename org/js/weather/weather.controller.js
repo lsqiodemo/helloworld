dd7705e8da354e6a125df081468e29d08113b57a
MagRelo <Magrelo404@gmail.com>
2014-12-16 14:18:48 -0700

    fix spinner?

--- org/js/weather/weather.controller.js
+++ org/js/weather/weather.controller.js
@@ -1,18 +1,22 @@
 angular.module("AngularTestApp")
   .controller('WeatherController', function($scope, $http) {
 
-    $scope.showLoader = false;
+    $scope.showSpinner = false;
     $scope.weather = null;
     $scope.city = "Boise";
     $scope.state = "ID";
 
     $scope.getWeather = function(){
-      $scope.showLoader = true;
+
+      $scope.showSpinner = true;
       $scope.weather = null;
+
       $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + $scope.city + '%2C%20' + $scope.state + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
         .then(function(result){
-          $scope.showLoader = false;
+
+          $scope.showSpinner = false;
           $scope.weather = result.data.query.results.channel;
+
         });
     };
 
