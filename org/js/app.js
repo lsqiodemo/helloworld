fb9f766b4a13cce4499640ffcf31e5fb9777a120
MagRelo <Magrelo404@gmail.com>
2014-12-16 10:21:36 -0700

    use https in weather call

--- org/js/app.js
+++ org/js/app.js
@@ -5,7 +5,7 @@ var myAppModule = angular.module('myApp', []);
 // in this example we will create a greeting filter
 myAppModule
   .controller('MainController', function($scope, $http) {
-    $http.get('http://api.openweathermap.org/data/2.5/weather?lat=46.5&lon=-116&units=imperial')
+    $http.get('https://api.openweathermap.org/data/2.5/weather?lat=46.5&lon=-116&units=imperial')
       .then(function(result){
 
         $scope.weather = result.data;
