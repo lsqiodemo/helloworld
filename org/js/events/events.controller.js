5b95f437046a0b05cdb544bb5fe7b70279077fc0
MagRelo <Magrelo404@gmail.com>
2014-12-19 12:33:33 -0700

    search and view events plus delete

--- org/js/events/events.controller.js
+++ org/js/events/events.controller.js
@@ -15,4 +15,58 @@ angular.module("AngularTestApp")
 
     };
 
+    $scope.addItem = function(collectionName, body){
+      $scope.showLoader = true;
+      $scope.error = null;
+
+      var runDate = new Date(body.Run_Date + " " + body.Run_Time);
+
+      var event = {
+        route: body.route,
+        date: runDate,
+        body: body.body
+      };
+
+      lsqService.addCollectionItem(collectionName, event)
+        .success(function(response){
+          $scope.showLoader = false;
+          $scope.resultName = collectionName;
+
+          //push in new item
+          $scope.events.push(response.result);
+        })
+        .error(function(response){
+          $scope.showLoader = false;
+          $scope.error = response.error;
+        });
+
+    };
+
+    $scope.removeItem = function(collectionName, id){
+      $scope.showLoader = true;
+      $scope.error = null;
+
+      lsqService.deleteCollectionItem(collectionName, id)
+        .success(function(response){
+          $scope.showLoader = false;
+          $scope.resultName = collectionName;
+
+          //splice out deleted item
+          angular.forEach($scope.events, function(item, index){
+            if(item._id == response.result._id){
+              $scope.events.splice(index, 1);
+            }
+          });
+        })
+        .error(function(response){
+          $scope.showLoader = false;
+          $scope.error = response.error;
+        });
+
+    };
+
+
+
+    //Init
+    $scope.getEvents();
   });
