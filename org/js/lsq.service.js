17d7bae77c5cb38d49980166bdd64e8a2c033208
MagRelo <Magrelo404@gmail.com>
2014-12-19 13:14:57 -0700

    add update method to lsq service

--- org/js/lsq.service.js
+++ org/js/lsq.service.js
@@ -6,12 +6,7 @@ angular.module('AngularTestApp')
   .factory('lsqService', function ($http) {
 
     var publicMethods = {};
-
-    var config = {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
+    var config = {headers: { "Content-Type": "application/json" }};
 
     var sendRequest = function(collectionName, requestType, query, model, select){
 
@@ -35,12 +30,23 @@ angular.module('AngularTestApp')
 
     publicMethods.addCollectionItem = function(collectionName, body){
 
-      return sendRequest(collectionName, "create", {}, body);
+      var query = {};
+
+      return sendRequest(collectionName, "create", query, body);
+    };
+
+    publicMethods.updateCollectionItem = function(collectionName, id, body){
+
+      var query = { "_id": id };
+
+      return sendRequest(collectionName, "update", query, body);
     };
 
     publicMethods.deleteCollectionItem = function(collectionName, id){
 
-      return sendRequest(collectionName, "delete", { "_id": id });
+      var query = { "_id": id };
+
+      return sendRequest(collectionName, "delete", query);
     };
 
     return publicMethods;
